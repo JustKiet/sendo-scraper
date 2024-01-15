863a34782a3965d3edb5e112c37387ec3e170fec
JustKiet <lunarthenerd@gmail.com>
2024-01-15 15:11:59 +0700

    v1.1

--- CNDPT.py
+++ CNDPT.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 def scrape_sendo():
     options = Options()
     options.headless = False  
-    driver = webdriver.Edge()
+    driver = webdriver.Edge(options=options)
     try:
         # Truy cập vào trang web
         driver.get("https://www.sendo.vn/")
@@ -41,14 +41,18 @@ def scrape_sendo():
             body.send_keys(Keys.PAGE_DOWN)
             body.send_keys(Keys.PAGE_DOWN)
             body.send_keys(Keys.PAGE_DOWN)
-            time.sleep(20)
+            time.sleep(2)
             body.send_keys(Keys.PAGE_DOWN)
             body.send_keys(Keys.PAGE_DOWN)
             title_element= driver.find_element(By.XPATH, "//h1[@class='d7ed-ytwGPk d7ed-zrT4k2 d7ed-kUYEit d7ed-AHa8cD d7ed-mzOLVa']")
-            summary_element = driver.find_element(By.XPATH, "//div[@class='_39ab-k3Gyep']")
-            rating_score_average = summary_element.find_element(By.XPATH, "//span[@class='d7ed-ChCxUf d7ed-AHa8cD d7ed-mzOLVa']")
-            rating_count = summary_element.find_element(By.XPATH, "//span[@class='_3141-BtwciV d7ed-KXpuoS d7ed-bjQW4F d7ed-ekib8m']")
+            title = "null"
+            score = "0"
+            rating_count = "0"
+            numbs = "null"
             try:
+                summary_element = driver.find_element(By.XPATH, "//div[@class='_39ab-k3Gyep']")
+                rating_score_average = summary_element.find_element(By.XPATH, "//span[@class='d7ed-ChCxUf d7ed-AHa8cD d7ed-mzOLVa']")
+                rating_count = summary_element.find_element(By.XPATH, "//span[@class='_3141-BtwciV d7ed-KXpuoS d7ed-bjQW4F d7ed-ekib8m']")
                 comments = driver.find_elements(By.XPATH, "//*[@id='id-danh-gia']/div/div[3]/div")
                 comment_contents = []  # List to store comment_content for all items
                 for comment in comments:
@@ -68,29 +72,28 @@ def scrape_sendo():
                     comment_content = comment_wrapper.find_element(By.TAG_NAME, "p")
                     comment_text = comment_content.text
                     comment_contents.append(comment_text)  # Append comment_text to the list
+                score = rating_score_average.text
+                numbs = rating_count.text 
+                title = title_element.text
+                row_data= {
+                    "TenSP":title,
+                    "DG":score,
+                    "SoDG":numbs,
+                    # "TenHienthi":username,
+                    # "verify":verify,
+                    #"comment_DG":star_score,
+                    "comment":comment_contents
+                }
+
+                data.append(row_data)
+                
+                driver.back()
+                time.sleep(3)
+                products_elements = driver.find_elements(By.CSS_SELECTOR, "div[class='d7ed-d4keTB d7ed-OoK3wU']")
             except NoSuchElementException:
-                pass
-            # Lấy nội dung của các phần tử khác
-            title = title_element.text
-            score = rating_score_average.text
-            numbs = rating_count.text 
-            # username = d_name_spans[0].text
-            # verify = d_name_spans[2].text
-
-            row_data={
-                "TenSP":title,
-                "DG":score,
-                "SoDG":numbs,
-                # "TenHienthi":username,
-                # "verify":verify,
-                #"comment_DG":star_score,
-                "comment":comment_contents
-            }
-
-            data.append(row_data)
-
-            driver.back()
-            products_elements = driver.find_elements(By.CSS_SELECTOR, "div[class='d7ed-d4keTB d7ed-OoK3wU']")
+                driver.back()
+                time.sleep(3)
+                products_elements = driver.find_elements(By.CSS_SELECTOR, "div[class='d7ed-d4keTB d7ed-OoK3wU']")
 
         # Kiểm tra xem tập tin đã tồn tại chưa để xác định việc ghi headers hay không
         write_headers = not os.path.exists("sendo.csv")
