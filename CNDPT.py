30083aa368d7271fe4d994111a1f02abd7a9a272
JustKiet <lunarthenerd@gmail.com>
2024-01-15 11:33:21 +0700

    Actual working crawler

--- CNDPT.py
+++ CNDPT.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.edge.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from csv import DictWriter
 import time
 import os
@@ -21,14 +22,15 @@ def scrape_sendo():
         search_element.send_keys("smartphone") 
         search_button = driver.find_element(By.XPATH, "//button[@class='d7ed-s0YDb1 d7ed-jQXTxb d7ed-AREzVq d7ed-YaJkXL d7ed-joBgy5 d7ed-DbNJxd']")
         search_button.click()
-        time.sleep(5)
+        time.sleep(10)
         current_page_url = driver.current_url  # Lưu trữ URL trang hiện tại
-        products_elements = driver.find_elements(By.XPATH, "//div[@class='d7ed-d4keTB d7ed-OoK3wU']")
+        products_wrapper = driver.find_element(By.XPATH, "//div[@class='d7ed-fdSIZS d7ed-OoK3wU d7ed-mPGbtR']")
+        products_elements = products_wrapper.find_elements(By.CSS_SELECTOR, "div[class='d7ed-d4keTB d7ed-OoK3wU']")
         data=[]
         comment_content = ""
         star_score = []
         d_name_spans = ""
-        for i in range(min(len(products_elements),1)):
+        for i in range(min(len(products_elements),6)):
             products_element = products_elements[i]
             title_link_element = products_element.find_element(By.TAG_NAME, 'a')
             link = title_link_element.get_attribute('href')
@@ -46,26 +48,28 @@ def scrape_sendo():
             summary_element = driver.find_element(By.XPATH, "//div[@class='_39ab-k3Gyep']")
             rating_score_average = summary_element.find_element(By.XPATH, "//span[@class='d7ed-ChCxUf d7ed-AHa8cD d7ed-mzOLVa']")
             rating_count = summary_element.find_element(By.XPATH, "//span[@class='_3141-BtwciV d7ed-KXpuoS d7ed-bjQW4F d7ed-ekib8m']")
-            comments = driver.find_elements(By.XPATH, "//*[@id='id-danh-gia']/div/div[3]/div")
-            comment_contents = []  # List to store comment_content for all items
-            for comment in comments:
-                star_container = comment.find_element(By.XPATH, "//div[@class='d7ed-P_QiIC d7ed-GE9WCQ']")
-                star_rating = star_container.find_element(By.XPATH, "//div[contains(@class, 'd7ed-ppmM09')]")
-                if star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-AYt6AP')]"):
-                    star_score.append('5')
-                if star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-vdoKHm')]"):
-                    star_score.append('4')
-                if star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-LrYHhb')]"):
-                    star_score.append('3')
-                if star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-tWkzSc')]"):
-                    star_score.append('2')
-                if star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-ygMGdU')]"):
-                    star_score.append('1')
-                comment_content = comment.find_element(By.XPATH, "//div[@class='_39ab-_2vzod']/p")
-                comment_text = comment_content.text
-                comment_contents.append(comment_text)  # Append comment_text to the list
-
-
+            try:
+                comments = driver.find_elements(By.XPATH, "//*[@id='id-danh-gia']/div/div[3]/div")
+                comment_contents = []  # List to store comment_content for all items
+                for comment in comments:
+                    #star_container = comment.find_element(By.CSS_SELECTOR, "div[class='d7ed-P_QiIC d7ed-GE9WCQ']")
+                    #star_rating = star_container.find_element(By.CSS_SELECTOR, "div.d7ed-ppmM09')]")
+                    #if star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-AYt6AP')]"):
+                    #    star_score.append('5')
+                    #elif star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-vdoKHm')]"):
+                    #    star_score.append('4')
+                    #elif star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-LrYHhb')]"):
+                    #    star_score.append('3')
+                    #elif star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-tWkzSc')]"):
+                    #    star_score.append('2')
+                    #elif star_rating.find_element(By.XPATH, "//div[contains(@class, 'd7ed-ygMGdU')]"):
+                    #    star_score.append('1')
+                    comment_wrapper = comment.find_element(By.CSS_SELECTOR, "div[class='_39ab-_2vzod']")
+                    comment_content = comment_wrapper.find_element(By.TAG_NAME, "p")
+                    comment_text = comment_content.text
+                    comment_contents.append(comment_text)  # Append comment_text to the list
+            except NoSuchElementException:
+                pass
             # Lấy nội dung của các phần tử khác
             title = title_element.text
             score = rating_score_average.text
@@ -79,14 +83,14 @@ def scrape_sendo():
                 "SoDG":numbs,
                 # "TenHienthi":username,
                 # "verify":verify,
-                "comment_DG":star_score,
+                #"comment_DG":star_score,
                 "comment":comment_contents
             }
 
             data.append(row_data)
 
             driver.back()
-            products_elements = driver.find_elements(By.XPATH, "//div[@class='d7ed-d4keTB d7ed-OoK3wU']")
+            products_elements = driver.find_elements(By.CSS_SELECTOR, "div[class='d7ed-d4keTB d7ed-OoK3wU']")
 
         # Kiểm tra xem tập tin đã tồn tại chưa để xác định việc ghi headers hay không
         write_headers = not os.path.exists("sendo.csv")
